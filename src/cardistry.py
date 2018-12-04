959ea615b0492b7c24bc61224d4779e67010d1e4
Lovac42 <lovac42@protonmail.com>
2018-12-04 17:59:19 -0500

    fixed sync disabling addon

--- src/cardistry.py
+++ src/cardistry.py
@@ -2,7 +2,7 @@
 # Copyright: (C) 2018 Lovac42
 # Support: https://github.com/lovac42/Cardistry
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
-# Version: 0.0.4
+# Version: 0.0.5
 
 
 # == User Config =========================================
@@ -53,15 +53,16 @@ if ANKI21:
     anki.schedv2.Scheduler._deckNewLimitSingle = wrap(anki.schedv2.Scheduler._deckNewLimitSingle, deckNewLimitSingle, 'around')
 
 
-#This fixes sync errors
-def onSync(type):
+#This monitor sync start/stops
+oldSync=anki.sync.Syncer.sync
+def onSync(self):
     global on_sync
-    if type=='finalize':
-        on_sync=False
-    else:
-        on_sync=True
-addHook('sync',onSync)
+    on_sync=True
+    ret=oldSync(self)
+    on_sync=False
+    return ret
 
+anki.sync.Syncer.sync=onSync
 
 
 ##################################################
