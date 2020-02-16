7ae5f0969659a2c01b8e1ed4d1ad62970206e710
lovac42 <lovac42@protonmail.com>
2020-02-16 00:53:30 -0500

    version bump v0.1.0

--- src/build.bat
+++ src/build.bat
@@ -2,7 +2,7 @@
 set ZIP=C:\PROGRA~1\7-Zip\7z.exe a -tzip -y -r
 set REPO=cardistry
 set PACKID=1535078906
-set VERSION=0.0.9
+set VERSION=0.1.0
 
 
 quick_manifest.exe "Cardistry: Dynamically Adjust New Cards" "%PACKID%" >%REPO%\manifest.json
