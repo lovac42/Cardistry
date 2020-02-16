5e2bcdba6431b13aaa3517a7b301dbf2d21c6cc6
lovac42 <lovac42@protonmail.com>
2020-02-16 00:27:56 -0500

    Updated copyright year and updated build script

---
+++ src/build.bat
@@ -0,0 +1,17 @@
+@echo off
+set ZIP=C:\PROGRA~1\7-Zip\7z.exe a -tzip -y -r
+set REPO=cardistry
+set PACKID=1535078906
+set VERSION=0.0.8
+
+
+quick_manifest.exe "Cardistry: Dynamically Adjust New Cards" "%PACKID%" >%REPO%\manifest.json
+echo %VERSION% >%REPO%\VERSION
+
+fsum -r -jm -md5 -d%REPO% * > checksum.md5
+move checksum.md5 %REPO%\checksum.md5
+
+%ZIP% %REPO%_v%VERSION%_Anki20.zip *.py %REPO%\*
+cd %REPO%
+%ZIP% ../%REPO%_v%VERSION%_Anki21.ankiaddon *
+%ZIP% ../%REPO%_v%VERSION%_CCBC.adze *
